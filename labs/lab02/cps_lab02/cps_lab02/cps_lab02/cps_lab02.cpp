00a89ad1e8c5f81f5d4b7b3f918563b4f8b39120
Tim-Surface <40219124@live.napier.ac.uk>
2018-10-01 14:26:11 +0100

    cps lab 2 start

---
+++ labs/lab02/cps_lab02/cps_lab02/cps_lab02/cps_lab02.cpp
@@ -0,0 +1,20 @@
+﻿// cps_lab02.cpp : Defines the entry point for the application.
+//
+
+#include "cps_lab02.h"
+#include <mutex>
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <condition_variable>
+
+using namespace std;
+
+void Monte_Carlo_Pi(int )
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
