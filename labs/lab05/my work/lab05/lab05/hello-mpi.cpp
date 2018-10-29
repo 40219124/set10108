b8ffcbdf2626b97b4f40c782b6cc049462f947eb
Emma Parsley <40206111@live.napier.ac.uk>
2018-10-29 15:58:54 +0000

    Lab 5 start.

---
+++ labs/lab05/my work/lab05/lab05/hello-mpi.cpp
@@ -0,0 +1,30 @@
+﻿// lab05.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+#include <mpi.h>
+
+using namespace std;
+
+int main()
+{
+	auto result = MPI_Init(nullptr, nullptr);
+	if (result != MPI_SUCCESS) {
+		cout << "ERROR - initialising MPI!" << endl;
+		MPI_Abort(MPI_COMM_WORLD, result);
+		return -1;
+	}
+
+	int num_procs, rank, length;
+	char host_name[MPI_MAX_PROCESSOR_NAME];
+	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Get_processor_name(host_name, &length);
+
+	cout << "Number of processors = " << num_procs << endl;
+	cout << "My rank = " << rank << endl;
+	cout << "Running on = " << host_name << endl;
+
+	MPI_Finalize();
+
+	return 0;
+}
